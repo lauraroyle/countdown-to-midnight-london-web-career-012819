285a536b7e25c071260ccee9e2cf80b91db57644
Laura Royle <lauraamie.royle@gmail.com>
2019-01-23 10:55:24 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(counter)
+  sleep(5)
 end
